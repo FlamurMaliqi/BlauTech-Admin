30c1167609f4d08f22596ea7f0ffee5c76ca8d4a
Flamur Maliqi <contact@flamur-maliqi.de>
2025-11-15 17:36:59 +0100

    test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,6 +19,7 @@ const CodeIcon = () => (
   </svg>
 )
 
+
 const GraduationIcon = () => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5z" />
